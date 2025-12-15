9ed60e5b5f0ca708d086e7ee2ce61fd5b68df91b
SplitLungs <erlachu@yahoo.com>
2025-12-14 22:02:24 -0600

    Release 1.0.0

---
+++ treeshaker/src/ShakerHeadItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vintagestory.API.Util;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+using Vintagestory.API.Common.Entities;
+
+namespace treeshaker
+{
+    public class ShakerHeadItem : Item
+    {
+        ICoreAPI Api;
+
+        public override void OnLoaded(ICoreAPI api)
+        {
+            base.OnLoaded(api);
+            Api = api;
+        }
+    }
+}
---
+++ treeshaker/src/TreeShakerMod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+
+namespace treeshaker
+{
+    public class TreeShakerMod : ModSystem
+    {
+        public ICoreAPI Api;
+        
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            Api = api;
+
+            Api.RegisterItemClass("TreeShakerItem", typeof(TreeShakerItem));
+            Api.RegisterItemClass("ShakerMotorItem", typeof(ShakerMotorItem));
+            Api.RegisterItemClass("ShakerHeadItem", typeof(ShakerHeadItem));
+        }
+    }
+}
